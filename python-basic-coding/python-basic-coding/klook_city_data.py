2c867c488ce1ff1f55dbf08ebd90053367397e7d
abhishek-malani <abhishekmalani3@gmail.com>
2019-09-04 19:24:38 +0530

    refactored code

---
+++ python-basic-coding/python-basic-coding/klook_city_data.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+url = "https://klook.klktech.com/signin"
+page_data = requests.get(url)
+page_source = BeautifulSoup(page_data.text, 'html.parser')
+tour_data = page_source.find('script')
+city_list_raw_data = tour_data.text.split(";")[3].strip().replace("var ", "")
+sliceobject = slice(15, None)
+city_list = city_list_raw_data[sliceobject]
+city_list_json = json.loads(city_list)
+city_dict = {}
+city_array = []
+for i in range(len(city_list_json)):
+    countries = city_list_json[i]['countries']
+    for j in range(len(countries)):
+        cities = countries[j]['cities']
+        for k in range(len(cities)):
+            city_dict = {"range_name": city_list_json[i]['range_name'], "country_name": city_list_json[i]['countries'][j]['country_name'],
+                         "city_id": city_list_json[i]['countries'][j]['cities'][k]['city_id'], "city_name": city_list_json[i]['countries'][j]['cities'][k]['city_name']}
+            city_array.append(city_dict)
+print(city_array)
